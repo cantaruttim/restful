3516798e60d9475b5b9e2266c953983a48bcc5fa
cantaruttim <cantaruttim@outlook.com>
2025-08-30 10:37:27 -0300

    atts columns

--- src/main/java/br/com/cantaruttim/model/Person.java
+++ src/main/java/br/com/cantaruttim/model/Person.java
@@ -15,10 +15,16 @@ public class Person implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    @Column(name = "first_name", nullable = false, length = 100)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 100)
     private String lastName;
+
+    @Column(nullable = false, length = 100)
     private String address;
+
+    @Column(nullable = false, length = 6)
     private String gender;
 
     public Person() {}
